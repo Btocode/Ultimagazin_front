3e68ddc9b934a179d3ebfc158182e682cbc4d7c9
Afsan Saeed <afsansaeed2017@gmail.com>
2023-05-14 23:58:47 +0600

    Logo Image url changed

--- src/Authentication/Index.jsx
+++ src/Authentication/Index.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import Login from "./Login";
 import Forgotpassword from "./Forgotpassword";
-import ForgotpasswordOTP from "./ForgotpasswordOTP";
+import Login from "./Login";
 // import Newpassword from "./Newpassword";
-import Signup from "./Signup";
+import SignUpSuccess from "./SignUpSuccess";
 import UserSignup from "./UserSignup";
-import SignUpSuccess from "./SignUpSuccess"
 
 const Index = () => {
   const [formLocation, setFormLocation] = useState(0);
@@ -25,31 +23,27 @@ const Index = () => {
   return (
     <div className="flex sm:flex-col md:flex-col lg:flex-col justify-center items-center h-screen">
       <div className="flex wrapper w-[80%] lg:w-full h-[70vh] sm:w-full items-center border-[2px] shadow-2xl md:flex-col sm:flex-col">
-
-     
-      
-      <div className="xl:flex xl:w-1/2 lg:w-1/3 w-full  justify-center items-center md:hidden sm:hidden">
-        <div className="w-full lg:h-screen xl:h-screen h-[300px]  flex items-center justify-center">
-          <img
-            className=""
-            src="../../../src/assets/images/logo3C.png"
-          />
+        <div className="xl:flex xl:w-1/2 lg:w-1/3 w-full  justify-center items-center md:hidden sm:hidden">
+          <div className="w-full lg:h-screen xl:h-screen h-[300px]  flex items-center justify-center">
+            <img
+              className=""
+              src="https://i.postimg.cc/zfR1hq5T/logo3C.png"
+              alt="login"
+            />
+          </div>
         </div>
-      </div>
-      <div className="div h-[50vh] bg-black border-[1px] md:hidden sm:hidden">
-
-      </div>
-      <div className="xl:w-1/2 lg:w-2/3 md:h-full w-full lg:h-screen xl:h-screen h-[500px] flex justify-center items-center">
-        <div
-          className={` rounded-3xl py-10 ${
-            formLocation <= 4 || formLocation > 5
-              ? "h-[560px] w-[700px]"
-              : "xl:h-[800px] xl:w-[850px] lg:h-[850px] lg:w-[800px] sm:h-[30vh] sm:w-[600px]"
-          } sm:w-full flex flex-col items-center justify-center`}>
-          {/* Login Form */}
-          {display[formLocation]}
+        <div className="div h-[50vh] bg-black border-[1px] md:hidden sm:hidden"></div>
+        <div className="xl:w-1/2 lg:w-2/3 md:h-full w-full lg:h-screen xl:h-screen h-[500px] flex justify-center items-center">
+          <div
+            className={` rounded-3xl py-10 ${
+              formLocation <= 4 || formLocation > 5
+                ? "h-[560px] w-[700px]"
+                : "xl:h-[800px] xl:w-[850px] lg:h-[850px] lg:w-[800px] sm:h-[30vh] sm:w-[600px]"
+            } sm:w-full flex flex-col items-center justify-center`}>
+            {/* Login Form */}
+            {display[formLocation]}
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
